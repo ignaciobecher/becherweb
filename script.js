2d62ba18d2c4ad7b42435e411c561b3218efeb0d
Ignacio <ignacio.becher@gmail.com>
2023-02-14 12:32:48 -0300

    navbar 50%

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const btnSend = document.getElementById("btn-submit-id");
 const btnMenu = document.getElementById("btn-menu");
 const btnClose = document.getElementById("btn-close");
-const headerLinks = document.querySelectorAll("#header-links");
+const headerLink1 = document.getElementById("header-links-1");
+const headerLink2 = document.getElementById("header-links-2");
+const headerLink3 = document.getElementById("header-links-3");
+const headerLink4 = document.getElementById("header-links-4");
 const headerResponsive = document.getElementById(
   "header-container-responsive-id"
 );
+const headerImg = document.getElementById("header-responsive-img");
 
 btnMenu.addEventListener("click", function () {
   btnMenu.style.display = "none";
@@ -12,8 +16,15 @@ btnMenu.addEventListener("click", function () {
   headerResponsive.style.gridTemplateRows = "auto";
   headerResponsive.style.gridTemplateColumns = "1fr 1fr";
   headerResponsive.style.justifyContent = "right";
-  headerLinks.style.display = "grid";
-  headerLinks.style.gridColumn = "2";
+  // headerImg.style.gridColumn = "1";
+  headerLink1.style.display = "grid";
+  headerLink1.style.gridColumn = "2";
+  headerLink2.style.display = "grid";
+  headerLink2.style.gridColumn = "2";
+  headerLink3.style.display = "grid";
+  headerLink3.style.gridColumn = "2";
+  headerLink4.style.display = "grid";
+  headerLink4.style.gridColumn = "2";
 });
 
 btnSend.addEventListener("click", function sendMail(e) {
@@ -25,10 +36,14 @@ btnSend.addEventListener("click", function sendMail(e) {
   };
   emailjs.send("service_6w8ishg", "template_cte5f6m", params).then(
     function (response) {
-      window.alert("SUCCESS!", response.status, response.text);
+      window.alert(
+        "Consulta enviada. A la brevedad nos pondremos en contacto con vos!",
+        response.status,
+        response.text
+      );
     },
     function (error) {
-      window.alert("FAILED...", error);
+      window.alert("Error al enviar formulario", error);
     }
   );
   e.preventDefault();
