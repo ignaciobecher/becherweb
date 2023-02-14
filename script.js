2d15916d7456cbc55eb2f5487a7fa35cf7149885
Ignacio <ignacio.becher@gmail.com>
2023-02-14 19:22:34 -0300

    navbar 100%

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const headerResponsive = document.getElementById(
   "header-container-responsive-id"
 );
 const headerImg = document.getElementById("header-responsive-img");
+const linkContainer = document.getElementById("links-container");
 
 btnMenu.addEventListener("click", function () {
   btnMenu.style.display = "none";
@@ -16,7 +17,11 @@ btnMenu.addEventListener("click", function () {
   headerResponsive.style.gridTemplateRows = "auto";
   headerResponsive.style.gridTemplateColumns = "1fr 1fr";
   headerResponsive.style.justifyContent = "right";
-  // headerImg.style.gridColumn = "1";
+  headerImg.style.gridColumn = "1";
+  headerImg.style.gridRow = "1";
+  linkContainer.style.display = "grid";
+  linkContainer.style.gridColumn = "2";
+  linkContainer.style.justifyItems = "end";
   headerLink1.style.display = "grid";
   headerLink1.style.gridColumn = "2";
   headerLink2.style.display = "grid";
@@ -25,6 +30,22 @@ btnMenu.addEventListener("click", function () {
   headerLink3.style.gridColumn = "2";
   headerLink4.style.display = "grid";
   headerLink4.style.gridColumn = "2";
+  btnClose.style.display = "grid";
+  btnClose.style.gridColumn = "2";
+  btnClose.style.gridRow = "1";
+  btnClose.style.alignSelf = "center";
+});
+
+btnClose.addEventListener("click", function () {
+  btnClose.style.display = "none";
+  btnMenu.style.display = "grid";
+  btnMenu.style.gridColumn = "2";
+  btnMenu.style.gridRow = "1";
+
+  headerLink1.style.display = "none";
+  headerLink2.style.display = "none";
+  headerLink3.style.display = "none";
+  headerLink4.style.display = "none";
 });
 
 btnSend.addEventListener("click", function sendMail(e) {
